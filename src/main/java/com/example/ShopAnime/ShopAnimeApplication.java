a40b2b8e6682446be4b1518061541a37f11b9bc3
hoangphong202 <hoangtrungphongit202@gmail.com>
2025-10-05 16:34:31 +0700

    first commit

---
+++ src/main/java/com/example/ShopAnime/ShopAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopAnime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShopAnime/ShopAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopAnime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
